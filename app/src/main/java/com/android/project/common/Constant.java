7e643668a3dba8a1f9612ebe9ba68bbb91876211
finn <992639429@qq.com>
2020-05-24 17:57:51 +0800

    Rename Constant

--- app/src/main/java/com/android/project/common/Constant.java
+++ app/src/main/java/com/android/project/common/Constant.java
@@ -5,7 +5,7 @@ package com.android.project.common;
  * @date 2020
  * 存放全局静态常量
  */
-public class Comparams {
+public class Constant {
 
     public static final String EXTRA_ACTION_TEST = "type_action_test";
 
--- app/src/main/java/com/android/project/ui/fragment/TestReposFragment.java
+++ app/src/main/java/com/android/project/ui/fragment/TestReposFragment.java
@@ -7,7 +7,7 @@ import android.widget.TextView;
 
 import com.android.project.R;
 import com.android.project.base.BaseMvpFragment;
-import com.android.project.common.Comparams;
+import com.android.project.common.Constant;
 import com.android.project.mvp.contract.TestContract;
 import com.android.project.mvp.model.bean.RepoBean;
 import com.android.project.mvp.presenter.TestPresenter;
@@ -40,7 +40,7 @@ public class TestReposFragment extends BaseMvpFragment<TestContract.View, TestPr
         super.initView(view);
         Bundle bundle = getArguments();
         if (bundle != null) {
-            String testStr = bundle.getString(Comparams.EXTRA_ACTION_TEST);
+            String testStr = bundle.getString(Constant.EXTRA_ACTION_TEST);
             ToastUtils.showToast(testStr);
         }
     }
